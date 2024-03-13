bd54e566ee4d132c18e20e8b54e87cb6669d7a74
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-03-13 13:18:21 -0300

    .

--- Codelândia/Desafio31/dist/slidesGroups.js
+++ Codelândia/Desafio31/dist/slidesGroups.js
@@ -1,23 +1,35 @@
+let index = 0;
 function handleClick(event) {
     const groups = document.querySelector('.groups');
     const group = document.querySelectorAll('.group');
+    const mobile = window.matchMedia("(max-width: 800px)").matches;
     if (event.currentTarget instanceof HTMLElement && groups && group) {
         if (event.currentTarget.getAttribute('data-control') === "prev") {
+            index - 1 < 0 ? index = 0 : index -= 1;
+        }
+        else {
+            mobile ? index + 1 >= group.length ? index = group.length - 1 : index += 1 : index + 5 >= group.length ? index = group.length - 5 : index += 1;
+        }
+        if (mobile) {
+            const { width } = group[0].getBoundingClientRect();
             group.forEach((item) => {
-                const { width } = item.getBoundingClientRect();
-                item.style.transform = `translateX(calc(+${width}px + 1rem))`;
+                item.style.transform = `translateX(calc(-${width * index}px - ${index > 0 ? index * 16 : 0}px))`;
             });
         }
         else {
+            const { width } = groups.getBoundingClientRect();
             group.forEach((item) => {
-                const { width } = item.getBoundingClientRect();
-                item.style.transform = `translateX(calc(-${width}px - 1rem))`;
+                item.style.transform = `translateX(calc(-${width * index}px - ${index > 1 ? 20 : 0}px))`;
             });
         }
     }
 }
 export function handleSlides() {
     const controlLeft = document.querySelectorAll('[data-control]');
+    window.addEventListener('resize', (event) => {
+        index = 0;
+        handleClick(event);
+    });
     if (controlLeft instanceof NodeList) {
         controlLeft.forEach((item) => item.addEventListener('click', handleClick));
     }
--- Codelândia/Desafio31/src/slidesGroups.ts
+++ Codelândia/Desafio31/src/slidesGroups.ts
@@ -1,27 +1,43 @@
+let index = 0;
+
 function handleClick(event: Event) {
   const groups = document.querySelector<HTMLElement>('.groups');
   const group = document.querySelectorAll<HTMLElement>('.group');
+  const mobile = window.matchMedia("(max-width: 800px)").matches;
+
   if (event.currentTarget instanceof HTMLElement && groups && group) {
     if(event.currentTarget.getAttribute('data-control') === "prev") {
-      group.forEach((item)=> {
-        const {width} = item.getBoundingClientRect();
-        item.style.transform = `translateX(calc(+${width}px + 1rem))`
-      });
+      index - 1 < 0 ? index = 0 : index -= 1;
     }
     else {
+      mobile ? index + 1 >= group.length ? index = group.length - 1 : index+=1 : index + 5 >= group.length ? index = group.length - 5 : index+=1;
+    }
+
+
+    if(mobile) {
+      const {width} = group[0].getBoundingClientRect();
       group.forEach((item)=> {
-        const {width} = item.getBoundingClientRect();
-        item.style.transform = `translateX(calc(-${width}px - 1rem))`
+        item.style.transform = `translateX(calc(-${width*index}px - ${index > 0 ? index * 16 : 0}px))`;
       });
     }
-    
+
+    else {
+      const {width} = groups.getBoundingClientRect();
+      group.forEach((item)=> {
+        item.style.transform = `translateX(calc(-${width*index}px - ${index > 1 ? 20 : 0}px))`;
+      });
+    }
   }
 }
 
 
 export function handleSlides() {
   const controlLeft = document.querySelectorAll('[data-control]');
-  
+  window.addEventListener('resize', (event)=> {
+    index = 0;
+    handleClick(event);
+  });
+
   if(controlLeft instanceof NodeList) {
     controlLeft.forEach((item) => item.addEventListener('click', handleClick));
   }
