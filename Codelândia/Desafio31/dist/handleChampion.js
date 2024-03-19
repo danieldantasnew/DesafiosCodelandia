7a4f8dbc8911ded5bee9cd35d3bb77bc99645579
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-03-19 11:28:03 -0300

    more details for worldChampion

--- Codelândia/Desafio31/dist/handleChampion.js
+++ Codelândia/Desafio31/dist/handleChampion.js
@@ -1,3 +1,18 @@
+function activeContainer() {
+    const content = document.querySelector('.contentWorld');
+    content?.classList.add('active');
+    activatedList();
+}
+function activatedList() {
+    const thropies = document.querySelectorAll('.trophy');
+    if (thropies) {
+        thropies.forEach((item, index) => {
+            setTimeout(() => item.classList.add('active'), 2000 * index);
+        });
+    }
+    ;
+}
 export function handleChampion() {
+    setTimeout(() => activeContainer(), 2000);
 }
 //# sourceMappingURL=handleChampion.js.map
--- Codelândia/Desafio31/src/handleChampion.ts
+++ Codelândia/Desafio31/src/handleChampion.ts
@@ -1,3 +1,19 @@
-export function handleChampion() {
+function activeContainer() {
+  const content = document.querySelector<HTMLElement>('.contentWorld');
+  content?.classList.add('active');
+  activatedList();
+}
 
+function activatedList() {
+  const thropies = document.querySelectorAll<Element>('.trophy');
+  if(thropies) {
+    thropies.forEach((item, index)=> {
+      setTimeout(()=> item.classList.add('active') , 2000 * index);
+    })
+  };
+}
+
+
+export function handleChampion() {
+  setTimeout(()=> activeContainer(), 2000)
 }
